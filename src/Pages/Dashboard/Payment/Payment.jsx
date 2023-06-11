00b6bf8ab22ab6010e78d086ea92a1dbf2dba4ed
Mahmudul Hasan Mahi <mahmudulhasan13189@gmail.com>
2023-06-11 19:26:27 +0600

    remaking payment

--- src/Pages/Dashboard/Payment/Payment.jsx
+++ src/Pages/Dashboard/Payment/Payment.jsx
@@ -1,20 +1,9 @@
-import { loadStripe } from "@stripe/stripe-js";
-import useCart from "../../../hooks/useCart";
-import { Elements } from "@stripe/react-stripe-js";
-import CheckoutForm from "./checkout/CheckOutForm";
+import React from 'react';
 
-
-// TODO: provide publishable Key
-const stripePromise = loadStripe(import.meta.env.VITE_Payment_Gateway_PK);
 const Payment = () => {
-  const [cart] = useCart();
-  const total = cart.reduce((sum, item) => sum + item.price, 0);
-  const price = parseFloat(total.toFixed(2));
   return (
-    <div className="w-100">
-      <Elements stripe={stripePromise}>
-        <CheckoutForm cart={cart} price={price}></CheckoutForm>
-      </Elements>
+    <div>
+      
     </div>
   );
 };
